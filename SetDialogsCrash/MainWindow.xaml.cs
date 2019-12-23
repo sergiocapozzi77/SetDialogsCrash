19c0f83912ae00c6d01c4597119e00cfe7a0c53b
Sergio Capozzi <sergiocapozzi@outlook.com>
2019-12-23 08:55:49 +0100

    first commit

--- SetDialogsCrash/MainWindow.xaml.cs
+++ SetDialogsCrash/MainWindow.xaml.cs
@@ -8,10 +8,10 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using LibVLCSharp.Shared;
 
 namespace SetDialogsCrash
 {
@@ -20,9 +20,27 @@ namespace SetDialogsCrash
     /// </summary>
     public partial class MainWindow : Window
     {
+        private LibVLC _libVLC;
+        private MediaPlayer _mediaPlayer;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            _libVLC = new LibVLC();
+            _libVLC.SetDialogHandlers((title, text) => Task.CompletedTask,
+                (dialog, title, text, username, store, token) => Task.CompletedTask,
+                (dialog, title, text, type, cancelText, actionText, secondActionText, token) => Task.CompletedTask,
+                (dialog, title, text, indeterminate, position, cancelText, token) => Task.CompletedTask,
+                (dialog, position, text) => Task.CompletedTask);
+            _mediaPlayer = new MediaPlayer(_libVLC);
+            VideoView.MediaPlayer = _mediaPlayer;
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            _mediaPlayer.Play(new Media(_libVLC,
+                "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/NOTEXISTINGURL.mp4", FromType.FromLocation));
         }
     }
 }
